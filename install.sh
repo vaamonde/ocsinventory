8f4a033def559dc484d438a7871d2be4d73f4480
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-08-31 08:56:44 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 31/05/2016
+# Data de atualização: 31/08/2016
+# Versão: 0.8
+# Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
+# Kernel >= 4.4.x
+#
+# Instalação do OCS Inventory Server
+#
+# Utilizar o comando: sudo -i para executar o script
+#
+# Caminho para o Log do install.sh
+LOG="/var/log/install.log"
+#
+# Variável da Data Inicial para calcular tempo de execução do Script
+DATAINICIAL=`date +%s`
+#
+# Validando o ambiente, verificando se o usuário e "root"
+USUARIO=`id -u`
+UBUNTU=`lsb_release -rs`
+KERNEL=`uname -r | cut -d'.' -f1,2`
+
+if [ "$USUARIO" == "0" ]
+then
+	if [ "$UBUNTU" == "16.04" ]
+		then
+			if [ "$KERNEL" == "4.4" ]
+				then
+					 clear
+					 echo -e "Usuário é `whoami`, continuando a executar o Install.sh"
+					 echo -e "Aguarde..."
+					 echo  ============================================================ >> $LOG
+					 
+					 echo -e "Atualizando as Listas do Apt-Get"
+					 #Exportando a variavel do Debian Frontend Noninteractive para não solicitar interação com o usuário
+					 export DEBIAN_FRONTEND=noninteractive
+					 #Atualizando as listas do apt-get
+					 apt-get update &>> $LOG
+					 echo -e "Listas Atualizadas com Sucesso!!!"
+					 echo  ============================================================ >> $LOG
+
+					 echo -e "Instalando os principais pacotes do OCS Inventory"
+					 #Instalação do principais pacotes do OCS Inventory, integrado com o Apache2 e MySQL
+
+					 echo -e "Instalação dos principais pacotes do OCS Invetory feito com sucesso!!!"
+					 echo  ============================================================ >> $LOG
+
+					 echo -e "Limpando o Cache do Apt-Get"
+					 #Limpando o diretório de cache do apt-get
+					 apt-get clean &>> $LOG
+					 echo -e "Cache Limpo com Sucesso!!!"
+					 echo ============================================================ >> $LOG
+					 
+					 echo >> $LOG
+					 echo -e "Fim do Install.sh em: `date`" >> $LOG
+					 echo
+					 echo -e "Instalação do OCS Inventory Feito com Sucesso!!!!!"
+					 echo
+					 # Script para calcular o tempo gasto para a execução do script-00.sh
+						 DATAFINAL=`date +%s`
+						 SOMA=`expr $DATAFINAL - $DATAINICIAL`
+						 RESULTADO=`expr 10800 + $SOMA`
+						 TEMPO=`date -d @$RESULTADO +%H:%M:%S`
+					 echo -e "Tempo gasto para execução do Install.sh: $TEMPO"
+					 echo -e "Pressione <Enter> para reinicializar o servidor: `hostname`"
+					 read
+					 sleep 2
+					 reboot
+					 else
+						 echo -e "Versão do Kernel: $KERNEL não homologada para esse script, versão: >= 4.4 "
+						 echo -e "Pressione <Enter> para finalizar o script"
+						 read
+			fi
+	 	 else
+			 echo -e "Distribuição GNU/Linux: `lsb_release -is` não homologada para esse script, versão: $UBUNTU"
+			 echo -e "Pressione <Enter> para finalizar o script"
+			 read
+	fi
+else
+	 echo -e "Usuário não é ROOT, execute o comando com a opção: sudo -i <Enter> depois digite a senha do usuário `whoami`"
+	 echo -e "Pressione <Enter> para finalizar o script"
+	read
+fi
