90e92fdc221e09ddf2551b4ad39fa3b51fe4e972
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-08-31 09:35:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,15 +32,23 @@ then
 			if [ "$KERNEL" == "4.4" ]
 				then
 					 clear
-					 # Variáveis de configuração da senha do MySQL é PhpMyAdmin
+					 # Variáveis de configuração da senha do MySQL
 					 PASSWORD="123456"
+ 					 
+ 					 # Variáveis de configuração do PhpMyAdmin
+					 APP_PASSWORD="123456"
+					 ADMIN_PASS="123456"
+					 APP_PASS="123456"
+					 WEBSERVER="apache2"
+					 ADMINUSER="root"
+
+					 #Exportando a variavel do Debian Frontend Noninteractive para não solicitar interação com o usuário
+					 export DEBIAN_FRONTEND=noninteractive
 					 
 					 echo -e "Usuário é `whoami`, continuando a executar o Install.sh"
 					 echo  ============================================================ >> $LOG
 					 
 					 echo -e "Atualizando as Listas do Apt-Get, aguarde..."
-					 #Exportando a variavel do Debian Frontend Noninteractive para não solicitar interação com o usuário
-					 export DEBIAN_FRONTEND=noninteractive
 					 #Atualizando as listas do apt-get
 					 apt-get update &>> $LOG
 					 echo -e "Listas Atualizadas com Sucesso!!!"
@@ -52,11 +60,120 @@ then
 					 echo "mysql-server-5.7 mysql-server/root_password password $PASSWORD" |  debconf-set-selections
 					 echo "mysql-server-5.7 mysql-server/root_password_again password $PASSWORD" |  debconf-set-selections
 					 #Instalando o LAMP Server completo e as dependêncais do OCS Inventory
-					 apt-get -y install lamp-server^ perl python &>> $LOG
-
+					 apt-get -y install lamp-server^ perl python make libapache2-mod-perl2 libapache2-mod-php snmp libio-compress-perl libxml-simple-perl libdbi-perl libdbd-mysql-perl libapache-dbi-perl libsoap-lite-perl libnet-ip-perl php-mysql php7.0-dev php-mbstring php-soap php7.0-zip php7.0-gd php7.0-mysql dmidecode libxml-simple-perl libcompress-raw-zlib-perl libnet-ip-perl libwww-perl libdigest-md5-file-perl libnet-ssleay-perl libcrypt-ssleay-perl libnet-snmp-perl libproc-pid-file-perl libproc-daemon-perl net-tools pciutils smartmontools read-edid nmap libc6-dev &>> $LOG
 					 echo -e "Instalação dos principais pacotes do OCS Invetory feito com sucesso!!!"
 					 echo  ============================================================ >> $LOG
 
+					 echo -e "Instalando o PhpMyAdmin, aguarde..."
+					 #Configurando as variaveis do Debconf para a instalação do PhpMyAdmin em modo Noninteractive
+					 echo "phpmyadmin phpmyadmin/internal/skip-preseed boolean true" |  debconf-set-selections
+					 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" |  debconf-set-selections
+					 echo "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASSWORD" |  debconf-set-selections
+					 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect $WEBSERVER" |  debconf-set-selections
+					 echo "phpmyadmin phpmyadmin/mysql/admin-user string $ADMINUSER" |  debconf-set-selections
+					 echo "phpmyadmin phpmyadmin/mysql/admin-pass password $ADMIN_PASS" |  debconf-set-selections
+					 echo "phpmyadmin phpmyadmin/mysql/app-pass password $APP_PASS" |  debconf-set-selections
+					 #Instalando o PhpMyAdmin
+					 apt-get -y install phpmyadmin php-mbstring php-gettext &>> $LOG
+					 #Atualizando as dependências do PhpMyAdmin, ativando os recursos de módulos do PHP no Apache2
+					 phpenmod mcrypt
+					 phpenmod mbstring
+					 #Reinicializando o serviço do Apache2 Server
+					 sudo service apache2 restart
+					 echo -e "Instalação do PhpMyAdmin Feito com Sucesso, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 echo ============================================================ >> $LOG
+
+					 echo -e "Instalação das Dependências do Perl XML::Entities via CPAN, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 #Instalação do XML::Entities					 
+					 perl -MCPAN -e 'install XML::Entities'
+					 echo -e "Instalação concluida com sucesso, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 
+					 echo -e "Instalação das Dependências do Perl SOAP::Lite via CPAN, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 #Instalação do SOAP::Lite					 
+					 perl -MCPAN -e 'install SOAP::Lite'
+					 echo -e "Instalação concluida com sucesso, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+
+					 echo -e "Instalação das Dependências do Perl XML::Simple via CPAN, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 #Instalação do XML::Simple					 
+					 perl -MCPAN -e 'install XML::Simple'
+					 echo -e "Instalação concluida com sucesso, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+
+					 echo -e "Instalação das Dependências do Perl Compress::Zlib via CPAN, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 #Instalação do Compress::Zlib					 
+					 perl -MCPAN -e 'install Compress::Zlib'
+					 echo -e "Instalação concluida com sucesso, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+
+					 echo -e "Instalação das Dependências do Perl DBD::Mysql via CPAN, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 #Instalação do DBD::Mysql					 
+					 perl -MCPAN -e 'install DBD::Mysql'
+					 echo -e "Instalação concluida com sucesso, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+
+					 echo -e "Instalação das Dependências do Perl Apache::DBI via CPAN, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 #Instalação do Apache::DBI					 
+					 perl -MCPAN -e 'install Apache::DBI'
+					 echo -e "Instalação concluida com sucesso, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+
+					 echo -e "Instalação das Dependências do Perl Net::IP via CPAN, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 #Instalação do Net::IP					 
+					 perl -MCPAN -e 'install Net::IP'
+					 echo -e "Instalação concluida com sucesso, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+
+					 echo -e "Instalação das Dependências do Perl Linux::Ethtool via CPAN, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 #Instalação do Linux::Ethtool					 
+					 perl -MCPAN -e 'install Linux::Ethtool'
+					 echo -e "Instalação concluida com sucesso, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+	
 					 echo -e "Limpando o Cache do Apt-Get"
 					 #Limpando o diretório de cache do apt-get
 					 apt-get clean &>> $LOG
