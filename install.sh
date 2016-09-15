090010381610c822863be2ad8042a5150d33ad39
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-09-15 13:40:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 31/05/2016
-# Data de atualização: 31/08/2016
-# Versão: 0.8
+# Data de atualização: 15/08/2016
+# Versão: 0.9
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
 #
