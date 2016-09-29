b67bdfc711ec052ac6fc48f97f56506c27ae6ce7
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-09-29 12:04:25 -0300

    Update and rename install.sh to 02-lamp.sh

--- 02-lamp.sh
+++ 02-lamp.sh
@@ -5,7 +5,7 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 31/05/2016
-# Data de atualização: 28/09/2016
+# Data de atualização: 29/09/2016
 # Versão: 0.10
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
