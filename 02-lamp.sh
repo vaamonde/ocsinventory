eab9912911e8e08f617b63633b141df6896a2519
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-09-29 12:09:52 -0300

    Update 02-lamp.sh

--- 02-lamp.sh
+++ 02-lamp.sh
@@ -14,8 +14,8 @@
 #
 # Utilizar o comando: sudo -i para executar o script
 #
-# Caminho para o Log do install.sh
-LOG="/var/log/install.log"
+# Caminho para o Log do lamp.sh
+LOG="/var/log/lamp.log"
 #
 # Variável da Data Inicial para calcular tempo de execução do Script
 DATAINICIAL=`date +%s`
@@ -42,21 +42,6 @@ then
 					 WEBSERVER="apache2"
 					 ADMINUSER="root"
 					 
-					 # Variáveis de configuração do OCS Inventory Server
-					 OCSVERSION="2.2.1/OCSNG_UNIX_SERVER-2.2.1.tar.gz"
-					 OCSTAR="OCSNG_UNIX_SERVER-2.2.1.tar.gz"
-					 OCSINSTALL="OCSNG_UNIX_SERVER-2.2.1"
-					 
- 					 # Variáveis de configuração do GLPI Help Desk
-					 GLPIVERSION="9.1/glpi-9.1.tar.gz"
-					 GLPITAR="glpi-9.1.tar.gz"
-					 GLPIINSTALL="glpi"
-					 
-					 #Variaǘeis de configuração do Plugin do OCS Inventory do GLPI
-					 GLPIOCSVERSION="1.2.3/glpi-ocsinventoryng-1.2.3.tar.gz"
-					 GLPIOCSTAR="glpi-ocsinventoryng-1.2.3.tar.gz"
-					 GLPIOCSINSTALL="ocsinventoryng"
-
 					 #Exportando a variável do Debian Frontend Noninteractive para não solicitar interação com o usuário
 					 export DEBIAN_FRONTEND=noninteractive
 					 
@@ -71,14 +56,14 @@ then
 					 echo
 					 echo  ============================================================ >> $LOG
 
-					 echo -e "Instalando os principais pacotes para o OCS Inventory - LAMP Server, aguarde..."
-					 #Instalação dos principais pacotes do OCS Inventory integrado com o Apache2 e MySQL
+					 echo -e "Instalando o LAMP Server (Linux, Apache, MySQL e PHP), aguarde..."
+					 #Instalação dos principais pacotes do OCS Inventory e do GLPI integrado com o Apache2 e MySQL
 					 #Configurando as variáveis do Debconf para a instalação do MySQL em modo Noninteractive
 					 echo "mysql-server-5.7 mysql-server/root_password password $PASSWORD" |  debconf-set-selections
 					 echo "mysql-server-5.7 mysql-server/root_password_again password $PASSWORD" |  debconf-set-selections
-					 #Instalando o LAMP Server completo e todas as dependêncais do OCS Inventory
+					 #Instalando o LAMP Server completo e todas as suas dependêncais do OCS Inventory e do GLPI
 					 apt-get -y install lamp-server^ perl python make libapache2-mod-perl2 libapache2-mod-php snmp libio-compress-perl libxml-simple-perl libdbi-perl libdbd-mysql-perl libapache-dbi-perl libsoap-lite-perl libnet-ip-perl php-mysql php7.0-dev php-mbstring php-soap php7.0-zip php7.0-gd php7.0-mysql dmidecode libxml-simple-perl libcompress-raw-zlib-perl libnet-ip-perl libwww-perl libdigest-md5-file-perl libnet-ssleay-perl libcrypt-ssleay-perl libnet-snmp-perl libproc-pid-file-perl libproc-daemon-perl net-tools pciutils smartmontools read-edid nmap libc6-dev php-pclzip gcc libarchive-zip-perl php7.0-json php7.0-mbstring php7.0-mysql php7.0-curl php7.0-gd php7.0-imap php7.0-ldap &>> $LOG
-					 echo -e "Instalação dos principais pacotes do OCS Inventory feito com sucesso!!!, continuando com o script."
+					 echo -e "Instalação do LAMP Server feito com sucesso!!!, continuando com o script."
 					 echo
 					 echo  ============================================================ >> $LOG
 
@@ -208,194 +193,14 @@ then
 					 sleep 2
 					 clear
 					 
-					 echo -e "Download do OCS Inventory Server do Github, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 #Fazendo o download do código fonte do OCS Inventory Server
-					 wget https://github.com/OCSInventory-NG/OCSInventory-ocsreports/releases/download/$OCSVERSION &>> $LOG
-					 #Descompactando o arquivos do OCS Inventory Server
-					 tar -zxvf $OCSTAR &>> $LOG
-					 #Acessando a pasta do OCS Inventory
-					 cd $OCSINSTALL
-					 echo -e "Download do OCS Inventory feito com sucesso, pressione <Enter> para instalar o OCS Inventory Server"
-					 echo
-					 echo -e "CUIDADO com as opções que serão solicitadas no decorrer da instalação."
-					 read
-					 clear
-					 #Executando a instalação do OCS Inventory Server e Reports
-					 ./setup.sh
-					 #MENSAGENS QUE SERÃO SOLICIDATAS NA INSTALAÇÃO DO OCS INVENTORY SERVER:
-					 #01. Do you wish to continue ([y]/n): y <-- digite y pressione <Enter>;
-					 #02. Which host is running database server [localhost]?: Deixe o padrão pressione <Enter>;
-					 #03. On which port is running database server [3306]?: Deixe o padrão pressione <Enter>;
-					 #04. Where is Apache daemon binary [/usr/sbin/apache2ctl]?: Deixe o padrão pressione <Enter>;
-					 #05. Where is Apache main configuration file [/etc/apache2/apache2.conf]?: Deixe o padrão pressione <Enter>;
-					 #06. Which user account is running Apache Web Server [www-data]?: Deixe o padrão pressione <Enter>;
-					 #07. Which user group is running Apache web server [www-data]?: Deixe o padrão pressione <Enter>;
-					 #08. Where is Apache Include configuration directory [/etc/apache2/conf-available]?: Deixe o padrão pressione <Enter>;
-					 #09. Where is PERL Intrepreter binary [/usr/bin/perl]?: Deixe o padrão pressione <Enter>;
-					 #10. Do you wish to setup Communication server on this computer ([y]/n)?: y <-- digite y pressione <Enter>;
-					 #11. Where to put Communication server log directory [/var/log/ocsinventory-server]?: Deixe o padrão pressione <Enter>;
-					 #12. Where to put Communication server plugins configuration files [/etc/ocsinventory-server/plugins]?: Deixe o padrão pressione <Enter>;
-					 #13. Where to put Communication server plugins Perl module files [/etc/ocsinventory-server/perl]?: Deixe o padrão pressione <Enter>;
-					 #MENSAGEM: NÃO SE PREOCUPE COM A MENSAGEM DE ERRO DO Apache2::SOAP PERL module..
-					 #14. Do you wish to continue ([y]/n)?: y <-- digite y pressione <Enter>;
-					 #15. Do you allow Setup renaming Communication Server Apache configuration file to 'z-ocsinventory-server.conf' ([y]/n)?: y <-- digite y pressione <Enter>;
-					 #16. Do you wish to setup Administration Server (Web Administration Console) on this computer ([y]/n)?: y <-- digite y pressione <Enter>;
-					 #17. Do you wish to continue ([y]/n)?: y <-- digite y pressione <Enter>;
-					 #18. Where to copy Administration Server static files for PHP Web Console [/usr/share/ocsinventory-reports]?: Deixe o padrão pressione <Enter>;
-					 #19. Where to create writable/cache directories for deployment packages administration console logs, IPDiscover and SNMP [/var/lib/ocsinventory-reports]?: Deixe o padrão pressione <Enter>;
-					 #APÓS A INSTALAÇÃO VIA NAVEGADOR, REMOVER O ARQUIVO install
-					 #Atualizando as informações do Apache2 para o suporte ao OCS Inventory Server e Reports
-					 a2dissite 000-default
-					 #Habilitando o conf do OCS Inventory Reports no Apache2
-					 a2enconf ocsinventory-reports
-					 #Habilitando o conf do OCS Inventory Server no Apache2
-					 a2enconf z-ocsinventory-server
-					 #Reinicializando o Apache2
-					 sudo service apache2 restart
-					 #Saindo do diretório do OCS Iventory
-					 cd ..
-					 echo
-					 echo -e "Instalação do OCS Inventory Server e Reports feito com sucesso, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 clear
-					 
-					 echo -e "Atualizando os arquivos de configuração do OCS Inventory"
-					 echo
-					 echo -e "Editando o arquivo do OCS Inventory Server, pressione <Enter> para continuar"
-					 #Arquivo de configuração do Servidor do OCS Inventory que vai receber as atualização do Clientes
-					 read
-					 #Fazendo o backup do arquivo de configuração original
-					 cp -v /etc/apache2/conf-available/z-ocsinventory-server.conf /etc/apache2/conf-available/z-ocsinventory-server.conf.bkp &>> $LOG
-					 #Atualizando para o novo arquivo de configuração
-					 cp -v conf/z-ocsinventory-server.conf /etc/apache2/conf-available/ &>> $LOG
-					 #Editando o arquivo de configuração
-					 vim /etc/apache2/conf-available/z-ocsinventory-server.conf
-					 echo -e "Arquivo editado com sucesso, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 clear
-					 
-					 echo -e "Editando o arquivo do OCS Inventory Server DBConfig, pressione <Enter> para continuar"
-					 #Arquivo de configuração para conexão com o Banco de Dados do MySQL
-					 #Configuração das variáveis de usuário e senha do banco de dados: database name (ocsweb) e user (ocs)
-					 read
-					 #Fazendo o backup do arquivo de configuração original
-					 cp -v /usr/share/ocsinventory-reports/ocsreports/dbconfig.inc.php /usr/share/ocsinventory-reports/ocsreports/dbconfig.inc.php.bkp &>> $LOG
-					 #Atualizando para o novo arquivo de configuração
-					 cp -v conf/dbconfig.inc.php /usr/share/ocsinventory-reports/ocsreports/ &>> $LOG
-					 #Editando o arquivo de configuração
-					 vim /usr/share/ocsinventory-reports/ocsreports/dbconfig.inc.php
-					 echo -e "Arquivo editado com sucesso, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 clear
-					 
-					 echo -e "Editando o arquivo do MySQL Server, pressione <Enter> para continuar"
-					 #Arquivo de configuração do Banco de Dados do MySQL Server
-					 #Permitir acesso aremoto ao MySQL comentando a linha: bind-address
-					 read
-					 #Fazendo o backup do arquivo de configuração original
-					 cp -v /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bkp &>> $LOG
-					 #Atualizando para o novo arquivo de configuração
-					 cp -v conf/mysqld.cnf /etc/mysql/mysql.conf.d/ &>> $LOG
-					 #Editando o arquivo de configuração
-					 vim /etc/mysql/mysql.conf.d/mysqld.cnf
-					 #Reinicializando o serviço do MySQL Server
-					 sudo service mysql restart
-					 echo -e "Arquivo editado com sucesso, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 clear
-					 
-					 echo -e "Editando o arquivo do PHP, pressione <Enter> para continuar"
-					 #Arquivo de configuração do PHP que será utilizado pelo Apache2
-					 #Aumentar os valores das váriaveis: post_max_size e upload_max_filesize para: 250MB
-					 read
-					 #Fazendo o backup do arquivo de configuração original
-					 cp -v /etc/php/7.0/apache2/php.ini /etc/php/7.0/apache2/php.ini.bkp &>> $LOG
-					 #Atualizando para o novo arquivos de configuração
-					 cp -v conf/php.ini /etc/php/7.0/apache2/ &>> $LOG
-					 #Editando o arquivo de configuração
-					 vim /etc/php/7.0/apache2/php.ini
-					 #Reinicializando o serviço do Apache2
-					 sudo service apache2 restart
-					 echo -e "Arquivo editado com sucesso, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 clear
-					 
-					 echo -e "Instalando o Cliente do OCS Inventory Agent, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 echo
-					 apt-get -y install ocsinventory-agent &>> $LOG
-					 echo -e "Instalação feita com sucesso, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 clear
-
-					 echo -e "Editando o arquivo do OCS Inventory Agent, pressione <Enter> para continuar"
-					 #Arquivo de configuração do OCS Agent (Cliente)
-					 read
-					 #Fazendo o backup do arquivo de configuração original
-					 cp -v /etc/ocsinventory/ocsinventory-agent.cfg /etc/ocsinventory/ocsinventory-agent.cfg.bkp &>> $LOG
-					 #Atualizando para o novo arquivo de configuração
-					 cp -v conf/ocsinventory-agent.cfg /etc/ocsinventory/ &>> $LOG
-					 #Editando o arquivo de configuração
-					 vim /etc/ocsinventory/ocsinventory-agent.cfg
-					 echo -e "Arquivo editado com sucesso, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 clear
-					 
-					 echo -e "Download do GLPI Help Desk do Github, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 #Fazendo o download do código fonte do GLPI Help Desk
-					 wget https://github.com/glpi-project/glpi/releases/download/$GLPIVERSION &>> $LOG
-					 #Descompactando o arquivos do GLPI Help Desk
-					 tar -zxvf $GLPITAR &>> $LOG
-					 #Movendo a pasta do GLPI Help Desk para /var/www/html/
-					 mv -v $GLPIINSTALL /var/www/html/ &>> $LOG
-					 #Fazendo o download do código fonte do Plugin do OCS Inventory
-					 wget https://github.com/pluginsGLPI/ocsinventoryng/releases/download/$GLPIOCSVERSION &>> $LOG
-					 #Descompactando o arquivo do Plugin do OCS Inventory
-					 tar -zxvf $GLPIOCSTAR &>> $LOG
-					 #Movendo a pasta do Plugin do OCS Inventory para o GLPI
-					 mv -v $GLPIOCSINSTALL /var/www/html/glpi/plugins/ &>> $LOG
-					 #Alterando as permissões de Dono e Grupo da pasta do GLPI Help Desk
-					 chown -Rf www-data.www-data /var/www/html/glpi/ &>> $LOG
-					 #MENSAGENS QUE SERÃO SOLICIDATAS NA INSTALAÇÃO DO GLPI HELP DESK VIA NAVEGADOR:
-					 #01. Selecione a linguage: Português do Brasil <OK>;
-					 #02. Licença: Eu li e ACEITO os termons de licença acima: <Continuar>;
-					 #03. Início da Instalação: <Instalar>;
-					 #04. Etapa 0: <Continuar>;
-					 #05. Etapa 1: localhost, root, 123456 <Continuar>;
-					 #06. Etapa 2: Selecione o banco de dados: glpi <Continuar>;
-					 #07. Etapa 3: <Continuar>;
-					 #08. Etapa 4: <Usar o GLPI>.
-					 #USUÁRIOS QUE SERÃO UTILIZADOS NO GLPI HELP DESK
-					 #glpi/glpi para a conta do usuário administrador
-    					 #tech/tech para a conta do usuário técnico
-    					 #normal/normal para a conta do usuário normal
-    					 #post-only/postonly para a conta do usuário postonly
-					 #APÓS A INSTALAÇÃO VIA NAVEGADOR, REMOVER A PASTA glpi/install
-					 echo -e "Download do GLPI feito com sucesso, pressione <Enter> para continuar"
-					 read
-					 sleep 2
-					 clear
-					 
-					 echo -e "Removendo aplicativos desnecessários"
+					 echo -e "Removendo aplicativos desnecessários, aguarde..."
 					 #Limpando o diretório de cache do apt-get
 					 apt-get autoremove &>> $LOG
-					 echo -e "Aplicativos removidos com Sucesso!!!"
+					 echo -e "Aplicativos removidos com Sucesso!!!, continuando com o script"
 					 echo
 					 echo ============================================================ >> $LOG
 
-					 echo -e "Limpando o Cache do Apt-Get"
+					 echo -e "Limpando o Cache do Apt-Get, aguarde..."
 					 #Limpando o diretório de cache do apt-get
 					 apt-get clean &>> $LOG
 					 echo -e "Cache Limpo com Sucesso!!!"
