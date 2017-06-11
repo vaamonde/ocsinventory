a36849e1c25744554b142952b62d22a9b10a804f
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-11 12:49:50 -0300

    Create 05-glpi.sh

--- 05-glpi.sh
+++ 05-glpi.sh
@@ -53,20 +53,28 @@ then
 					 echo -e "Download do GLPI Help Desk do Github, pressione <Enter> para continuar"
 					 read
 					 sleep 2
+					 
 					 #Fazendo o download do código fonte do GLPI Help Desk
 					 wget https://github.com/glpi-project/glpi/releases/download/$GLPIVERSION &>> $LOG
+					 
 					 #Descompactando o arquivos do GLPI Help Desk
 					 tar -zxvf $GLPITAR &>> $LOG
+					 
 					 #Movendo a pasta do GLPI Help Desk para /var/www/html/
 					 mv -v $GLPIINSTALL /var/www/html/ &>> $LOG
+					 
 					 #Fazendo o download do código fonte do Plugin do OCS Inventory Server
 					 wget https://github.com/pluginsGLPI/ocsinventoryng/releases/download/$GLPIOCSVERSION &>> $LOG
+					 
 					 #Descompactando o arquivo do Plugin do OCS Inventory
 					 tar -zxvf $GLPIOCSTAR &>> $LOG
+					 
 					 #Movendo a pasta do Plugin do OCS Inventory para o GLPI
 					 mv -v $GLPIOCSINSTALL /var/www/html/glpi/plugins/ &>> $LOG
+					 
 					 #Alterando as permissões de Dono e Grupo da pasta do GLPI Help Desk
 					 chown -Rf www-data.www-data /var/www/html/glpi/ &>> $LOG
+					 
 					 #MENSAGENS QUE SERÃO SOLICIDATAS NA INSTALAÇÃO DO GLPI HELP DESK VIA NAVEGADOR:
 					 #01. Selecione a linguage: Português do Brasil <OK>;
 					 #02. Licença: Eu li e ACEITO os termons de licença acima: <Continuar>;
@@ -76,12 +84,17 @@ then
 					 #06. Etapa 2: Selecione o banco de dados: glpi <Continuar>;
 					 #07. Etapa 3: <Continuar>;
 					 #08. Etapa 4: <Usar o GLPI>.
+					 
 					 #USUÁRIOS QUE SERÃO UTILIZADOS NO GLPI HELP DESK
 					 #glpi/glpi para a conta do usuário administrador
     				 	 #tech/tech para a conta do usuário técnico
     				 	 #normal/normal para a conta do usuário normal
     				 	 #post-only/postonly para a conta do usuário postonly
+					 
 					 #APÓS A INSTALAÇÃO VIA NAVEGADOR, REMOVER A PASTA glpi/install
+					 #cd /var/www/html/glpi/
+					 #rm -Rf install
+					 
 					 echo -e "Download do GLPI feito com sucesso, pressione <Enter> para continuar"
 					 read
 					 sleep 2
