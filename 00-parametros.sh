851f63465e45415fe900d1d66bdc81dbe587ae76
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-17 15:54:06 -0300

    Update 00-parametros.sh

--- 00-parametros.sh
+++ 00-parametros.sh
@@ -10,5 +10,13 @@
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
 #
-# Parâmetros (variaveis de ambiente) utilizados nos scripts de instalação do OCS Inventory, GLPI e Netdata
+# Parâmetros (variáveis de ambiente) utilizados nos scripts de instalação do OCS Inventory, GLPI e Netdata
+#
+# Variável da Data Inicial para calcular o tempo de execução dos Scripts
+DATAINICIAL=`date +%s`
+#
+# Variáveis de Validação do ambiente, verificando se o usuário e "root", versão do "ubuntu" e versão do "kernel"
+USUARIO=`id -u`
+UBUNTU=`lsb_release -rs`
+KERNEL=`uname -r | cut -d'.' -f1,2`
 #
