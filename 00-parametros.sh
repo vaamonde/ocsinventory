103be8ec6734ac0f2cf65e6127a63e5627fa3573
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-17 15:52:13 -0300

    Create 00-parametros.sh

---
+++ 00-parametros.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 17/06/2017
+# Data de atualização: 17/06/2017
+# Versão: 0.1
+# Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
+# Kernel >= 4.4.x
+#
+# Parâmetros (variaveis de ambiente) utilizados nos scripts de instalação do OCS Inventory, GLPI e Netdata
+#
