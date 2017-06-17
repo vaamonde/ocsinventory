6a07179c07df37f5e38e9942d0ab0aa2429f0f3b
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-17 16:14:26 -0300

    Update 06-netdata.sh

--- 06-netdata.sh
+++ 06-netdata.sh
@@ -17,13 +17,7 @@
 # Caminho para o Log do netdata.sh
 LOG="/var/log/netdata.log"
 #
-# Variável da Data Inicial para calcular tempo de execução do Script
-DATAINICIAL=`date +%s`
-#
-# Validando o ambiente, verificando se o usuário e "root"
-USUARIO=`id -u`
-UBUNTU=`lsb_release -rs`
-KERNEL=`uname -r | cut -d'.' -f1,2`
+
 
 if [ "$USUARIO" == "0" ]
 then
@@ -32,11 +26,6 @@ then
 			if [ "$KERNEL" == "4.4" ]
 				then
 					 clear
-					 
-					 #Variáveis de configuração do Netdata
-					 NETDATAVERSION="netdata.git"
-					 NETDATAINSTALL="netdata"
-
 					 #Exportando a variável do Debian Frontend Noninteractive para não solicitar interação com o usuário
 					 export DEBIAN_FRONTEND=noninteractive
 					 
