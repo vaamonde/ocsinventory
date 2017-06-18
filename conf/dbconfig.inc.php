c816eb6ffaa2c6dfd6f93fda85cd7e9aa8656ecb
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-18 16:02:42 -0300

    Update dbconfig.inc.php

--- conf/dbconfig.inc.php
+++ conf/dbconfig.inc.php
@@ -1,7 +1,22 @@
 <?php
+// Autor: Robson Vaamonde Site: www.procedimentosemti.com.br
+// Facebook: facebook.com/ProcedimentosEmTI
+// Facebook: facebook.com/BoraParaPratica
+// YouTube: youtube.com/BoraParaPratica
+// Data de criação: 31/05/2016
+// Data de atualização: 17/06/2017
+// Versão: 0.11
+// Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
+// Kernel >= 4.4.x
+
+// Nome da Base de Dados do OCS Iventory Server
 define("DB_NAME", "ocsweb");
+// Nome do Servidor de Leitura da Base de Dados do OCS Inventory Server
 define("SERVER_READ","localhost");
+// Nome do Servidor de Escrita da Base de Dados do OCS Invetory Server
 define("SERVER_WRITE","localhost");
+// Nome do Usuaŕio de autenticação na Base de Dados do OCS Iventory Server
 define("COMPTE_BASE","ocs");
+// Senha do Usuário de autenticação na Base de Dados do OCS Iventory Server
 define("PSWD_BASE","123456"); 
 ?>
