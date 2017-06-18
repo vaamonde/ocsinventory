758b11b0179400b91a89304caf62aa3c2982ff15
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-18 15:13:21 -0300

    Create 04-pos_ocs_server.sh

---
+++ 04-pos_ocs_server.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 31/05/2016
+# Data de atualização: 17/06/2017
+# Versão: 0.11
+# Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
+# Kernel >= 4.4.x
+#
+# Instalação do OCS Inventory Server
+# Instalação do OCS Inventory Reports
+#
+# Utilizar o comando: sudo -i para executar o script
+#
+# Caminho para o Log do ocs_server.sh
+LOG="/var/log/ocs_server.log"
+#
+#Arquivo de configuração de parâmetros
+source 00-parametros.sh
+
+if [ "$USUARIO" == "0" ]
+then
+	if [ "$UBUNTU" == "16.04" ]
+		then
+			if [ "$KERNEL" == "4.4" ]
+				then
+					 clear
+					 
+					 echo -e "Usuário é `whoami`, continuando a executar o ocs_server.sh"
+					 #Exportando a variável do Debian Frontend Noninteractive para não solicitar interação com o usuário
+					 export DEBIAN_FRONTEND=noninteractive
+					 echo
+					 echo  ============================================================ >> $LOG
+					 
+
+					 echo ============================================================ >> $LOG
+
+					 echo -e "Fim do pos_ocs_server.sh em: `date`" >> $LOG
+					 echo -e "Finalização da Pos Instalação do OCS Inventory Server Feito com Sucesso!!!!!"
+					 echo
+					 # Script para calcular o tempo gasto para a execução do ocs_server.sh
+						 DATAFINAL=`date +%s`
+						 SOMA=`expr $DATAFINAL - $DATAINICIAL`
+						 RESULTADO=`expr 10800 + $SOMA`
+						 TEMPO=`date -d @$RESULTADO +%H:%M:%S`
+					 echo -e "Tempo gasto para execução do ocs_server.sh: $TEMPO"
+					 echo -e "Pressione <Enter> para reinicializar o servidor: `hostname`"
+					 read
+					 sleep 2
+					 reboot
+					 else
+						 echo -e "Versão do Kernel: $KERNEL não homologada para esse script, versão: >= 4.4 "
+						 echo -e "Pressione <Enter> para finalizar o script"
+						 read
+			fi
+	 	 else
+			 echo -e "Distribuição GNU/Linux: `lsb_release -is` não homologada para esse script, versão: $UBUNTU"
+			 echo -e "Pressione <Enter> para finalizar o script"
+			 read
+	fi
+else
+	 echo -e "Usuário não é ROOT, execute o comando com a opção: sudo -i <Enter> depois digite a senha do usuário `whoami`"
+	 echo -e "Pressione <Enter> para finalizar o script"
+	read
+fi
