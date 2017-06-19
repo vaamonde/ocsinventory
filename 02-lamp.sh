eefaa6bb5377bdbe1931459b3ec7cad009d81532
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-19 14:08:24 -0300

    Update 02-lamp.sh

--- 02-lamp.sh
+++ 02-lamp.sh
@@ -364,6 +364,18 @@ then
 					 read
 					 sleep 2
 					 clear
+					 
+					 echo -e "Instalação das Dependências do APCU do GLPI via Pecl, pressione <Enter> para continuar"
+					 read
+					 
+					 #Instalação do apcu-bc
+					 #Mensagem:  ? [no] <-- pressione <Enter>
+					 pecl install apcu_bc-beta
+					 
+					 echo -e "Instalação concluída com sucesso!!!, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
 
 					 echo -e "Editando o arquivo do Apache2, pressione <Enter> para continuar"
 					 read
