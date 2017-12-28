15d555e6d7c0dc85e11197790206491b25ab7b72
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-12-27 22:08:16 -0200

    Update 03-ocs_server.sh

--- 03-ocs_server.sh
+++ 03-ocs_server.sh
@@ -5,7 +5,7 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 31/05/2016
-# Data de atualização: 17/06/2017
+# Data de atualização: 27/12/2017
 # Versão: 0.11
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
