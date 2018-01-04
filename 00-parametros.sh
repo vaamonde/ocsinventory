23edfc9077583387d860c4850fee9ea15af56f3f
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-01-04 12:02:39 -0200

    Update 00-parametros.sh

--- 00-parametros.sh
+++ 00-parametros.sh
@@ -5,12 +5,20 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 17/06/2017
-# Data de atualização: 30/12/2017
-# Versão: 0.2
+# Data de atualização: 04/01/2018
+# Versão: 0.3
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
 #
 # Parâmetros (variáveis de ambiente) utilizados nos scripts de instalação do OCS Inventory, GLPI e Netdata
+#
+
+# Variável do caminho do Log dos Script utilizado nesse curso
+VARLOGPATH="/var/log/ocsinstall/"
+#
+
+# Variável para criação do arquivo de Log dos Script
+LOGSCRIPT=`echo $0 | cut -d'/' -f2`
 #
 
 # Variável da Data Inicial para calcular o tempo de execução dos Scripts
