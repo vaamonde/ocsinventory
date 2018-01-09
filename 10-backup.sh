c03f76247ec7518e5cfc9cdc533bf5389998c72f
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-01-09 17:32:48 -0200

    Create 10-backup.sh

---
+++ 10-backup.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 07/01/2018
+# Data de atualização: 07/01/2018
+# Versão: 0.1
+# Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
+# Kernel >= 4.4.x
+#
+# Instalação do Fusion Inventory Server e Agent com integração com GLPI
+#
+# Utilizar o comando: sudo -i para executar o script
+#
+
+# Arquivo de configuração de parâmetros
+source 00-parametros.sh
+#
+
+# Caminho para o Log do script
+LOG=$VARLOGPATH/$LOGSCRIPT
+#
+
+if [ "$USUARIO" == "0" ]
+then
+	if [ "$UBUNTU" == "16.04" ]
+		then
+			if [ "$KERNEL" == "4.4" ]
+				then
+					 clear
+					 
+					 echo -e "Usuário é `whoami`, continuando a executar o $LOGSCRIPT"
+					 #Exportando a variável do Debian Frontend Noninteractive para não solicitar interação com o usuário
+					 export DEBIAN_FRONTEND=noninteractive
+					 echo
+					 echo  ============================================================ &>> $LOG
+					 
+					 echo -e "Instalação do sistema de Inventário de Rede Fusion Inventory"
+					 echo -e "Pressione <Enter> para instalar"
+					 read
+					 sleep 2
+					 echo
+					 
+
+					 
+           				 echo  ============================================================ >> $LOG
+                     
+					 echo -e "Fim do $LOGSCRIPT em: `date`" &>> $LOG
+					 echo -e "Instalação do Netdata feito com Sucesso!!!!!"
+					 echo
+					 # Script para calcular o tempo gasto para a execução do netdata.sh
+						 DATAFINAL=`date +%s`
+						 SOMA=`expr $DATAFINAL - $DATAINICIAL`
+						 RESULTADO=`expr 10800 + $SOMA`
+						 TEMPO=`date -d @$RESULTADO +%H:%M:%S`
+					 echo -e "Tempo gasto para execução do netdata.sh: $TEMPO"
+					 echo -e "Pressione <Enter> para reinicializar o servidor: `hostname`"
+					 read
+					 sleep 2
+					 reboot
+					 else
+						 echo -e "Versão do Kernel: $KERNEL não homologada para esse script, versão: >= 4.4 "
+						 echo -e "Pressione <Enter> para finalizar o script"
+						 read
+			fi
+	 	 else
+			 echo -e "Distribuição GNU/Linux: `lsb_release -is` não homologada para esse script, versão: $UBUNTU"
+			 echo -e "Pressione <Enter> para finalizar o script"
+			 read
+	fi
+else
+	 echo -e "Usuário não é ROOT, execute o comando com a opção: sudo -i <Enter> depois digite a senha do usuário `whoami`"
+	 echo -e "Pressione <Enter> para finalizar o script"
+	read
+fi
