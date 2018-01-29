a2174ce9b0b66ac5bfe80da0120e479e67ee48f5
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-01-29 13:59:34 -0200

    Update glpibackup.sh

--- conf/glpibackup.sh
+++ conf/glpibackup.sh
@@ -5,8 +5,8 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 09/01/2018
-# Data de atualização: 09/01/2018
-# Versão: 0.12
+# Data de atualização: 29/01/2018
+# Versão: 0.13
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
 
@@ -15,32 +15,32 @@ HOST="localhost"
 USER="root"
 PWD="123456"
 DB="ocsweb"
-PATH="/backup/glpi"
+PATH_1="/backup/glpi"
 DATA=`date +%d_%m_%Y-%H_%M`
-LOG="$PATH/backup.log"
+LOG="$PATH_1/backup.log"
 
 #Verificando se o diretório de Backup existe
-if [ -d $PATH ]; then
-	echo -e "Diretório de Log existe, continuando o backup..." &>> $LOG
+if [ -d $PATH_1 ]; then
+	echo -e "Diretório de Log existe, continuando o backup..."
 else
-	echo -e "Diretório de Log não existe, criando o diretório." &>> $LOG
-	mkdir -pv $PATH  &>> $LOG
-	echo -e "Direttório criado sucesso!!!!, continuando o backup..." &>> $LOG
+	echo -e "Diretório de Log não existe, criando o diretório."
+	mkdir -pv $PATH_1
+	echo -e "Diretório criado sucesso!!!!, continuando o backup..."
 fi
 
 #Reinicializando o serviço do MySQL
 sudo service mysql restart
 
 echo -e "Backup da Base de Dados: $DB" &>> $LOG
-mysqldump --add-drop-table --complete-insert --extended-insert --quote-names --host=$HOST --user=$USER --password=$PWD --databases $DB > $PATH/glpi-$DATA.sql
+mysqldump --add-drop-table --complete-insert --extended-insert --quote-names --host=$HOST --user=$USER --password=$PWD --databases $DB > $PATH_1/glpi-$DATA.sql
 echo -e "Backup feito com sucesso!!!, continuando o backup..." &>> $LOG
 
 echo -e "Compactando o Backup da Base de Dados: $DB" &>> $LOG
-tar -zcvf $PATH/glpi-$DATA.tar.gz $PATH/glpi-$DATA.sql &>> $LOG
+tar -zcvf $PATH_1/glpi-$DATA.tar.gz $PATH_1/glpi-$DATA.sql &>> $LOG
 echo -e "Compactação feita com sucesso!!!, continuando o backup..." &>> $LOG
 
 echo -e "Removendo o arquivo SQL da Base Dados: $DB" &>> $LOG
-rm -v $PATH/*.sql &>> $LOG
+rm -v $PATH_1/*.sql &>> $LOG
 echo -e "Arquivo removido com sucesso!!!, continuando o backup..." &>> $LOG
 
 echo -e "Backup do $DB finalizado com sucesso!!!" &>> $LOG
