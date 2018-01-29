6ca29fbd8c7101ba1f38e7e8e3cab3e9cf51d82c
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-01-29 13:58:14 -0200

    Update ocsbackup.sh

--- conf/ocsbackup.sh
+++ conf/ocsbackup.sh
@@ -15,32 +15,32 @@ HOST="localhost"
 USER="root"
 PWD="123456"
 DB="ocsweb"
-PATH="/backup/ocsinventory"
+PATH_1="/backup/ocsinventory"
 DATA=`date +%d_%m_%Y-%H_%M`
-LOG="$PATH/backup.log"
+LOG="$PATH_1/backup.log"
 
 #Verificando se o diretório de Backup existe
-if [ -d $PATH ]; then
-	echo -e "Diretório de Log existe, continuando o backup..." &>> $LOG
+if [ -d $PATH_1 ]; then
+	echo -e "Diretório de Log existe, continuando o backup..."
 else
-	echo -e "Diretório de Log não existe, criando o diretório." &>> $LOG
-	mkdir -pv $PATH &>> $LOG
-	echo -e "Direttório criado sucesso!!!!, continuando o backup..." &>> $LOG
+	echo -e "Diretório de Log não existe, criando o diretório."
+	mkdir -pv $PATH_1
+	echo -e "Direttório criado sucesso!!!!, continuando o backup..."
 fi
 
 #Reinicializando o serviço do MySQL
 sudo service mysql restart
 
 echo -e "Backup da Base de Dados: $DB" &>> $LOG
-mysqldump --add-drop-table --complete-insert --extended-insert --quote-names --host=$HOST --user=$USER --password=$PWD --databases $DB > $PATH/ocsweb-$DATA.sql
+mysqldump --add-drop-table --complete-insert --extended-insert --quote-names --host=$HOST --user=$USER --password=$PWD --databases $DB > $PATH_1/ocsweb-$DATA.sql
 echo -e "Backup feito com sucesso!!!, continuando o backup..." &>> $LOG
 
 echo -e "Compactando o Backup da Base de Dados: $DB" &>> $LOG
-tar -zcvf $PATH/ocsweb-$DATA.tar.gz $PATH/ocsweb-$DATA.sql &>> $LOG
+tar -zcvf $PATH_1/ocsweb-$DATA.tar.gz $PATH_1/ocsweb-$DATA.sql &>> $LOG
 echo -e "Compactação feita com sucesso!!!, continuando o backup..." &>> $LOG
 
 echo -e "Removendo o arquivo SQL da Base Dados: $DB" &>> $LOG
-rm -v $PATH/*.sql &>> $LOG
+rm -v $PATH_1/*.sql &>> $LOG
 echo -e "Arquivo removido com sucesso!!!, continuando o backup..." &>> $LOG
 
 echo -e "Backup do $DB finalizado com sucesso!!!" &>> $LOG
