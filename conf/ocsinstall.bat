462eb90cf6b7163387c43d214415d6fec4baa734
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-02-05 14:13:19 -0200

    Create ocsinstall.bat

---
+++ conf/ocsinstall.bat
@@ -0,0 +1,32 @@
+@echo off
+rem Autor: Robson Vaamonde
+rem Site: www.procedimentosemti.com.br
+rem Facebook: facebook.com/ProcedimentosEmTI
+rem Facebook: facebook.com/BoraParaPratica
+rem YouTube: youtube.com/BoraParaPratica
+rem Data de criação: 05/01/2018
+rem Data de atualização: 05/01/2018
+rem Versão: 0.1
+cls
+
+echo Script de Instalação do OCS Inventory Agent
+echo Utilização do agent de forma automatizada
+echo Data/hora de inicio do processo de instalação: %date% - %time%
+
+rem Parametros de utilização do OCS Inventory Agent
+rem /server=https://ocs.pti.intra/ocsinventory - Caminho do Servidor OCS Inventory
+rem /ssl=1 - Habilitar o SSL
+rem /ca="ocs.crt" - Caminho e nome do arquivo de Certificado
+rem /tag="Desktop" - Nome da TAG
+rem /debug=1 - Habilitar o Modo de Debug (Detalhado)
+rem /force - Força a atualização do inventario
+
+echo Iniciando do processo de Instalação do OCS Inventory Agent
+OCS-NG-Windows-Agent-Setup.exe /server=https://ocs.pti.intra/ocsinventory /ssl=1 /ca="ocs.crt" /tag="Desktop" /debug=1
+
+echo Copiando o Arquivo de Certificado SSL
+copy ocs.crt "c:\ProgamData\OCS Inventory NG\Agent"
+
+echo Fim do processo de Instalação do OCS Inventory Agent:
+echo Data: %date% - hora: %time%
+pause
