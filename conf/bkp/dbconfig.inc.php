b139b4c9ef8913aec72295d9264e70adea8a51e8
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-06-12 10:43:07 -0300

    Add files via upload

---
+++ conf/bkp/dbconfig.inc.php
@@ -0,0 +1,23 @@
+<?php
+// Autor: Robson Vaamonde Site: www.procedimentosemti.com.br
+// Script utilizado no Curso de OCS Inventory Server
+// Facebook: facebook.com/ProcedimentosEmTI
+// Facebook: facebook.com/BoraParaPratica
+// YouTube: youtube.com/BoraParaPratica
+// Data de criação: 31/05/2016
+// Data de atualização: 31/12/2017
+// Versão: 0.11
+// Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
+// Kernel >= 4.4.x
+
+// Nome da Base de Dados do OCS Iventory Server
+define("DB_NAME", "ocsweb");
+// Nome do Servidor de Leitura da Base de Dados do OCS Inventory Server
+define("SERVER_READ","localhost");
+// Nome do Servidor de Escrita da Base de Dados do OCS Invetory Server
+define("SERVER_WRITE","localhost");
+// Nome do Usuaŕio de autenticação na Base de Dados do OCS Iventory Server
+define("COMPTE_BASE","ocs");
+// Senha do Usuário de autenticação na Base de Dados do OCS Iventory Server
+define("PSWD_BASE","123456"); 
+?>
