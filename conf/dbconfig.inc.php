cbea4a15171224e89c5cc3090717c61adc89f047
Robson Vaamonde <robsonvaamonde@gmail.com>
2020-11-08 17:34:33 -0300

    Atualizacao

--- conf/dbconfig.inc.php
+++ conf/dbconfig.inc.php
@@ -1,10 +1,28 @@
 <?php
+// Autor: Robson Vaamonde Site: www.procedimentosemti.com.br
+// Script utilizado no Curso de OCS Inventory Server
+// Facebook: facebook.com/ProcedimentosEmTI
+// Facebook: facebook.com/BoraParaPratica
+// YouTube: youtube.com/BoraParaPratica
+// Data de criação: 31/05/2016
+// Data de atualização: 08/11/2020
+// Versão: 0.12
+// Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
+// Kernel >= 4.4.x
+
+// Nome da Base de Dados do OCS Inventory Server
 define("DB_NAME", "ocsweb");
+// Nome do Servidor de Leitura da Base de Dados do OCS Inventory Server
 define("SERVER_READ","localhost");
+// Nome do Servidor de Escrita da Base de Dados do OCS Invetory Server
 define("SERVER_WRITE","localhost");
+// Porta de Conexão do Banco de Dados do MySQL do OCS Inventory Server
 define("SERVER_PORT","3306");
+// Nome do Usuário de autenticação na Base de Dados do OCS Inventory Server
 define("COMPTE_BASE","ocs");
+// Senha do Usuário de autenticação na Base de Dados do OCS Iventory Server
 define("PSWD_BASE","123456");
+// Configurações do Suporte ao SSL do OCS Inventory Server
 define("ENABLE_SSL","0");
 define("SSL_MODE","");
 define("SSL_KEY","");
