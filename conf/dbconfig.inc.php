c4fca77f44ba092f6147ec3f7c66ec0e09723508
root <root@ptispo01is01.pti.intra>
2020-11-08 17:12:35 -0300

    Atualizacao

--- conf/dbconfig.inc.php
+++ conf/dbconfig.inc.php
@@ -1,23 +1,13 @@
 <?php
-// Autor: Robson Vaamonde Site: www.procedimentosemti.com.br
-// Script utilizado no Curso de OCS Inventory Server
-// Facebook: facebook.com/ProcedimentosEmTI
-// Facebook: facebook.com/BoraParaPratica
-// YouTube: youtube.com/BoraParaPratica
-// Data de criação: 31/05/2016
-// Data de atualização: 31/12/2017
-// Versão: 0.11
-// Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
-// Kernel >= 4.4.x
-
-// Nome da Base de Dados do OCS Iventory Server
 define("DB_NAME", "ocsweb");
-// Nome do Servidor de Leitura da Base de Dados do OCS Inventory Server
 define("SERVER_READ","localhost");
-// Nome do Servidor de Escrita da Base de Dados do OCS Invetory Server
 define("SERVER_WRITE","localhost");
-// Nome do Usuaŕio de autenticação na Base de Dados do OCS Iventory Server
+define("SERVER_PORT","3306");
 define("COMPTE_BASE","ocs");
-// Senha do Usuário de autenticação na Base de Dados do OCS Iventory Server
-define("PSWD_BASE","123456"); 
+define("PSWD_BASE","123456");
+define("ENABLE_SSL","0");
+define("SSL_MODE","");
+define("SSL_KEY","");
+define("SSL_CERT","");
+define("CA_CERT","");
 ?>
